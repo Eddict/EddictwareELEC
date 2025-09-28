14c6dfa67add621aef8b228610c4c8baf3e7aabe
Eddict <microsoft@eddict.nl>
2025-09-28 03:44:33 +0200

    $GITHUB_WORKSPACE in bash

--- .github/actions/scripts/set_git_config.sh
+++ .github/actions/scripts/set_git_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 touch $HOME/.gitconfig
 
-echo "Set git safe.directory: ${{ github.workspace }}"
-git config --global --unset-all safe.directory "${{ github.workspace }}"
-git config --global --add safe.directory "${{ github.workspace }}"
+echo "Set git safe.directory: $GITHUB_WORKSPACE"
+git config --global --unset-all safe.directory "$GITHUB_WORKSPACE"
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
-echo "Set git init.defaultBranch: ${{ github.event.repository.default_branch }}"
+#echo "Set git init.defaultBranch: ${{ github.event.repository.default_branch }}"
 # https://github.com/actions/checkout/issues/430#issuecomment-810950736
-git config --global init.defaultBranch ${{ github.event.repository.default_branch }}
+#git config --global init.defaultBranch ${{ github.event.repository.default_branch }}
