851201cf49e451979a7a5de27b8ebed4e4229c5e
Eddict <microsoft@eddict.nl>
2025-10-17 04:02:09 +0200

    test

--- .github/actions/javascript/with-post-step/post.js
+++ .github/actions/javascript/with-post-step/post.js
@@ -1,2 +1,4 @@
-console.log("INPUT_POST: " + process.env.INPUT_POST);
+console.log(`INPUT_POST: ${INPUT_POST}`);
+console.log(`INPUT_POST: ` + process.env.INPUT_POST);
+console.log(`INPOST: ${STATE_INPOST}`);
 console.log("INPOST: " + process.env.STATE_INPOST);
