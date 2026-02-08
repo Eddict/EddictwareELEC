67547e76f949eca865579ad8b40acb383946cd9f
Eddict <microsoft@eddict.nl>
2026-02-08 11:28:35 +0100

    restored missing EOL

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -12,6 +12,7 @@ PKG_DEPENDS_TARGET="toolchain libtirpc"
 PKG_DEPENDS_INIT="toolchain libtirpc"
 PKG_LONGDESC="BusyBox combines tiny versions of many common UNIX utilities into a single small executable."
 PKG_BUILD_FLAGS="-parallel +lto +size"
+
 pre_build_target() {
   PKG_MAKE_OPTS_TARGET="ARCH=${TARGET_ARCH} \
                         HOSTCC=${HOST_CC} \
